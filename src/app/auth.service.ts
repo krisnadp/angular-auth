d4f791f32b95450627eb6d97e042eeaca2881da6
krisnadp <krisna10permana@gmail.com>
2024-05-01 14:07:54 +0700

    feat: initial commit angular auth

---
+++ src/app/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+export class AuthService {
+ 
+  // @Injectable({
+  //   providedIn: 'root'
+  // })
+
+//   type AuthResponse = {
+//     id: number;
+//     username: string;
+//     email: string;
+//     firstName: string;
+//     lastName: string;
+//     gender: string;
+//     image: string;
+//     token: string;
+// }
+
+
+  // @Injectable({
+  //   providedIn: 'root'
+  // });
+
+    // private const baseUrl = 'https://dummyjson.com/auth/login';
+
+  // constructor(private http: HttpClient) { }
+
+  // login(){
+  //   username: String;
+  //   password: String;
+  // }
+
+}
